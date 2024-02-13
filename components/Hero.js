f573356e72938076794e7ee1f690ec8afc085533
Raunak Pandey <praunak82@gmail.com>
2024-02-13 11:21:23 +0530

    image issue

--- components/Hero.js
+++ components/Hero.js
@@ -7,15 +7,14 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="relative flex flex-col w-full min-h-screen">
-      <img
+      <Image
         src={
-          "https://s3-alpha-sig.figma.com/img/964f/da4c/227c0861d3991136f0d2f2a2700d7a0a?Expires=1707696000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=JmWX9hg5F61Y1lWtDNerSG~LmDmOfHz3kA2aNQS8vhyr4POeWrPkVvFszSSg7oI0LKVfnDyaFHcLbMO8~tevHwhfgCjQBflVJXARLFhpwVR0TwHQPgVbLZSLM2ISNjuVdsMh90QzYm~MZxtfz44DnIFFjMIMyXJEmPEkIaopxhU48WJgzOYZrIs3tS8w0JQbZbj2AbkaG1oT-7bbxKkr4zqiAB9QRKkJvjAJyodFCOxe4JcZgVUyev1G5fvm3LeHMfKIjbxs1yYchN4jqw4ALNYgEaf8TDCI9W6BzQ10cAi0JJHeAmoN6bC3RftYsjF2Di-nQ~x21o9MSX-UPXZU6w__"
+         require("../public/Desktop.svg")
         }
         alt=""
         loading="eager"
         className="absolute inset-0 object-cover w-full h-full -z-10"
       />
-      <div className="absolute inset-0 object-cover w-full h-full -z-[5] bg-black/50"></div>
       <div className="w-5/6 mx-auto my-10 z-10 flex justify-between items-center">
         <Image src={require("@/public/bfm_logo.svg")} alt="" />
         <button className="">
